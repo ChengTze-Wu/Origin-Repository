f33752a3744db707186ee97eca9ee80a747173a3
ChengTze-Wu <yaoop3050777@gmail.com>
2022-06-08 23:13:32 +0800

    dev 0.0.1

--- model/member.py
+++ model/member.py
@@ -11,24 +11,24 @@ config = {
     'database': os.environ['DB_DATABASE']
 }
 
-cnx = mysql.connector.connect(pool_name = "user",
-                              pool_size = 5,
+cnx = mysql.connector.connect(pool_name="user",
+                              pool_size=5,
                               **config)
+
 
 def get_current_user(email):
     try:
-        cnx = mysql.connector.connect(pool_name = "user")
+        cnx = mysql.connector.connect(pool_name="user")
         cursor = cnx.cursor()
-        
+
         query_user = ("select id, name, email from users "
                       "where email = %s")
         data_user = (email,)
-            
+
         cursor.execute(query_user, data_user)
         data = cursor.fetchone()
-        result = {"id":data[0], "name":data[1], "email":data[2]} if data else None
-        
-        return {"data":result}
+        result = {"id": data[0], "name": data[1],
+                  "email": data[2]} if data else None
     except Exception as e:
         raise e
     finally:
@@ -36,20 +36,21 @@ def get_current_user(email):
             cnx.rollback()
         cursor.close()
         cnx.close()
+        return {"data": result}
+
 
 def signup_user(name, email, password):
     try:
-        cnx = mysql.connector.connect(pool_name = "user")
+        cnx = mysql.connector.connect(pool_name="user")
         cursor = cnx.cursor()
-        
+
         add_user = ("INSERT INTO users "
                     "(name, email, password) "
                     "VALUES (%s, %s, %s)")
         data_user = (name, email, password)
-        
+
         cursor.execute(add_user, data_user)
         cnx.commit()
-        return True
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_DUP_ENTRY:
             return False
@@ -60,20 +61,21 @@ def signup_user(name, email, password):
             cnx.rollback()
         cursor.close()
         cnx.close()
+        return True
+
 
 def signin_user(email, password):
     try:
-        cnx = mysql.connector.connect(pool_name = "user")
+        cnx = mysql.connector.connect(pool_name="user")
         cursor = cnx.cursor()
-        
+
         query_user = ("select email from users "
                       "where email = %s and password = %s")
         data_user = (email, password)
-       
+
         cursor.execute(query_user, data_user)
         data = cursor.fetchone()
         result = data[0] if data else None
-        return result
     except Exception as e:
         raise e
     finally:
@@ -81,3 +83,4 @@ def signin_user(email, password):
             cnx.rollback()
         cursor.close()
         cnx.close()
+        return result
