25ee322e9fb007561d4f5cfd8d260d25458c8177
ChengTze-Wu <yaoop3050777@gmail.com>
2022-06-20 16:22:29 +0800

    change to rds

--- model/attraction.py
+++ model/attraction.py
@@ -7,6 +7,7 @@ config = {
     'user': os.environ['DB_USER'],
     'password': os.environ['DB_PASSWORD'],
     'host': os.environ['DB_HOST'],
+    'port': os.environ['DB_PORT'],
     'database': os.environ['DB_DATABASE']
 }
 
--- model/member.py
+++ model/member.py
@@ -8,6 +8,7 @@ config = {
     'user': os.environ['DB_USER'],
     'password': os.environ['DB_PASSWORD'],
     'host': os.environ['DB_HOST'],
+    'port': os.environ['DB_PORT'],
     'database': os.environ['DB_DATABASE']
 }
 
